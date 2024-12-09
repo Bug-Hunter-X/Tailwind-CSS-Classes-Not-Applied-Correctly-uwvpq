72b0d357e7b5beed1b881a0e5b27436a309df213
BugHunter-X <askzenai@gmail.com>
2024-12-09 18:16:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,10 @@
+```javascript
+<div class="flex">
+  <div class="bg-red-500 p-4">
+    <p>This div should be red.</p>
+  </div>
+  <div class="bg-blue-500 p-4">
+    <p>This div should be blue.</p>
+  </div>
+</div>
+```
